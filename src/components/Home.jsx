9cf435912adc4dc6e20f6d1eb678171b3afeb9c0
MbuviDev3 <mbuvidev@gmail.com>
2025-12-19 09:06:47 +0300

    updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -53,7 +53,7 @@ const featuredProducts = products.slice(0, 3); // show only 3
 
 
       {/* Services Grid */}
-      <div className="py-16 lg:py-24 bg-gray-30">
+      <div className="py-16 lg:py-24 ">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-md transition">
@@ -94,7 +94,7 @@ const featuredProducts = products.slice(0, 3); // show only 3
       
 
    {/* Why Choose Section */}
-<div className="py-8 lg:py-10 bg-gray-30">
+<div className="py-8 lg:py-10 ">
   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
     <div className="text-center mb-12">

4b55abbefe544e2cb4fd996d699e34e09bae4118
MbuviDev3 <mbuvidev@gmail.com>
2025-12-19 16:04:02 +0300

    am

--- src/components/data/products.js
+++ src/components/data/products.js
@@ -55,8 +55,8 @@ const products = [
       {
     id: 7,
     name: "24KW Solar System",
-    price: 500,
-    oldPrice: 500,
+    price: 50,
+    oldPrice: 100,
     description: "24KW Solar System suitable for normal home usage. HO Green, save more.",
     images: ["/images/24KW Solar System.jpeg", "/images/24KW Solar System 2.jpeg"],
     paystackLink: "https://paystack.com/buy/24kw-solar-system-xawclc"
